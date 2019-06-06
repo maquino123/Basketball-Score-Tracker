212dd95a9f0350c2fc5e9350c0cfbbc07da79e1c
Marlon Aquino <marlon.aquino24@gmail.com>
2019-06-06 15:19:32 -0700

    update scores when button is clicked for team A

--- BasketballScoreTracker/app/src/main/java/com/example/android/basketballscoretracker/MainActivity.java
+++ BasketballScoreTracker/app/src/main/java/com/example/android/basketballscoretracker/MainActivity.java
@@ -7,24 +7,45 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Score is a global variable as it needs to be
+    // - saved
+    // - modified for a longer period of time
+    // - in between button clicks
+    int scoreForTeamA = 0;
+
     @Override
     //Occurs right when app starts up
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(0);
+        displayForTeamA(scoreForTeamA);
     }
 
+    /**
+     * Increases the score for Team A by 3
+     * @param v
+     */
     public void addThreeForTeamA(View v){
-        displayForTeamA(3);
+        scoreForTeamA = scoreForTeamA + 3;
+        displayForTeamA(scoreForTeamA);
     }
 
+    /**
+     * Increases the score for Team A by 2
+     * @param v
+     */
     public void addTwoForTeamA(View v){
-        displayForTeamA(2);
+        scoreForTeamA = scoreForTeamA + 2;
+        displayForTeamA(scoreForTeamA);
     }
 
+    /**
+     * Increases the score for Team A by 1
+     * @param v
+     */
     public void addFreeThrowForTeamA(View v){
-        displayForTeamA(1);
+        scoreForTeamA = scoreForTeamA + 1;
+        displayForTeamA(scoreForTeamA);
     }
 
 
